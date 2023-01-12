858f39457ebf20695f53b9bb8aade019d212f159
DeviousWings <nksteele@byui.edu>
2023-01-11 18:16:56 -0600

    small changed

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Nathanael King's Portfolio Updated</h1>
+        <h1>Nathanael King's Portfolio</h1>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
