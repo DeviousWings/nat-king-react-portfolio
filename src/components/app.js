3292c64593190d88148aa8dddd2e6944871dafbb
DeviousWings <nksteele@byui.edu>
2023-01-11 18:08:49 -0600

    edited title/added moment

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Nathanael King's Portfolio Updated</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
