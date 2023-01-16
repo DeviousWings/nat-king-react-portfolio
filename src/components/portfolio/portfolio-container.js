5e77d0f32329304cd721e4049a1e9c47531399b4
DeviousWings <nksteele@byui.edu>
2023-01-16 12:33:42 -0500

    took out hard code to rely more on devcamp.space

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,80 +1,79 @@
 import React, { Component } from "react";
-import axios from 'axios';
-
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-items";
 
-
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
-        
-        this.state = {
-            pageTitle: "Wlecome to my Portfolio",
-            isLoading: false,
-            data: []
-        };
-        this.handleFilter = this.handleFilter.bind(this);
+  constructor() {
+    super();
 
-    }
+    this.state = {
+      pageTitle: "Wlecome to my Portfolio",
+      isLoading: false,
+      data: [],
+    };
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-    handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter((item) => {
+        return item.category === filter;
+      }),
+    });
+  }
 
-    getPortfolioItems() {
-        axios
-        .get('https://natking.devcamp.space/portfolio/portfolio_items')
-        .then(response =>  {
+  getPortfolioItems() {
+    axios
+      .get("https://natking.devcamp.space/portfolio/portfolio_items")
+      .then((response) => {
         // handle success
-          console.log("response data", response);
-          this.setState({
-            data: response.data.portfolio_items
-          })
-        })
-        .catch(error => {
-          console.log(error);
-          
+        console.log("response data", response);
+        this.setState({
+          data: response.data.portfolio_items,
         });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+
+    return this.state.data.map((item) => {
+      return (
+        <PortfolioItem
+          key={item.id}
+          item={item}
+        />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            
-          return (
-          <PortfolioItem title={item.name} url={item.url} slug={item.id}/>
-          );
-        });
-      }
-      
-      componentDidMount() {
-          this.getPortfolioItems();
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-      }
-
-    render() {
-        if (this.state.isLoading) {
-
-            return <div>Loading...</div>
-        }
-
-        return(
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('Contractor')}>Contractor</button>
-                <button onClick={() => this.handleFilter('Private Sector')}>Private Sector</button>
-                <button onClick={() => this.handleFilter('Goverment')}>Goverment</button>
-
-
-                {this.portfolioItems()}
-
-
-            </div>
-            )
-    }
+        <button onClick={() => this.handleFilter("Contractor")}>
+          Contractor
+        </button>
+        <button onClick={() => this.handleFilter("Private Sector")}>
+          Private Sector
+        </button>
+        <button onClick={() => this.handleFilter("Goverment")}>
+          Goverment
+        </button>
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
--- src/components/portfolio/portfolio-items.js
+++ src/components/portfolio/portfolio-items.js
@@ -2,12 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
+    //Data that we will need:
+    // - background image: thumb_image_url
+    // - logo
+    // - description: description
+    // - id: id
+    const { id, description, thumb_image_url, logo } = props.item;
     return (
         <div>
-        <h3>{props.title}</h3>
-        <h4>{props.url}</h4>
-
-        <Link to={`/portfolio/${props.slug}`}>Project</Link>
+        {/* This will do the same but looks cleaner and is more relient on the website devcamp.space/ */}
+        <img src={thumb_image_url}/>
+        <img src={logo}/>
+            <div>
+                {description}
+            </div>
+            <Link to={`/portfolio/${id}`}>Project</Link>
+        {/* Step one was to see if you could get access to the API */}
+        {/* <Link to={`/portfolio/${props.slug}`}>Project</Link> */}
         </div>
         )
 }
