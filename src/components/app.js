796719c7110dcbe9d140301612d05ff6869591b2
DeviousWings <nksteele@byui.edu>
2023-01-18 17:01:03 -0500

    adding authentication

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,9 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
+
 export default class App extends Component {
 
  
@@ -25,6 +27,7 @@ export default class App extends Component {
         <NavigationContainer />
         <Switch>
           <Route exact path="/" component ={Home} />
+          <Route exact path="/auth" component ={Auth} />
           <Route exact path="/about-me" component ={About} />
           <Route exact path="/contact" component ={Contact} />
           <Route exact path="/blog" component ={Blog} />
