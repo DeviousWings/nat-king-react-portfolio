e1981ae3128e937a2845c70d8ff4936f0bc6369d
DeviousWings <nksteele@byui.edu>
2023-01-27 16:38:39 -0500

    fixed images

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -4,17 +4,17 @@ const PortfolioSidebarList = (props) => {
 
     const portfolioList = props.data.map(portfolioItem => {
         return (
-        <div>
-            <div>
+        <div className="portfolio-item-thumb">
+            <div className="portfolio-thumb-img">
                 <img src={portfolioItem.thumb_image_url}></img>
             </div>
-            <h1>{portfolioItem.name}</h1>
+            <h1 className='title'>{portfolioItem.name}</h1>
             <h1>{portfolioItem.id}</h1>
          </div>
         )
     })
 
-    return <div>{portfolioList}</div>;
+    return <div className='portfolio-sidebar-list-wrapper'>{portfolioList}</div>;
 }
 
 export default PortfolioSidebarList;
