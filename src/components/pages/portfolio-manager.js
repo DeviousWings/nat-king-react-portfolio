d9d7b435d176eb7aa0fa57ef16ecc82f44654524
DeviousWings <nksteele@byui.edu>
2023-01-27 16:15:37 -0500

    Built out portfolio manager

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PortfolioManager extends Component {
-    render() {
-        return (
-        <div>
-            <h1>Portfolio Manager</h1>
-            <h1>Portfolio Manager</h1>
+    constructor( ){
+        super();
 
-        </div>
-        );
+        this.state = {
+            portfolioItems: []
+        }
+        
+        }
+
+
+    getPortfolioItems() {
+        axios.get("https://natking.devcamp.space/portfolio/portfolio_items", { 
+            withCredentials: true
+          }).then(response => {
+            this.setState({
+                portfolioItems:[...response.data.portfolio_items]
+            })
+          }).catch(error =>{
+            console.log('error in getPortfolioItems', error);
+            
+          })
     }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    render() {
+
+        return (
+            <div className="portfolio-manager-wrapper">
+                <div className='left-column'>
+                    <h1>Portfolio form...</h1>
+                </div>
+
+                <div className='right-column'>
+                    <h1>Portfolio columns...</h1>
+                </div>
+
+
+            </div>
+            );
+        }
+    
 }
