996b4bb4ef7bd9d4ebc246ed9650ca0cd71d1222
DeviousWings <nksteele@byui.edu>
2023-01-28 13:01:57 -0500

    port-form creation

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
+import PortfolioForm from '../portfolio/portfolio-form';
 
 export default class PortfolioManager extends Component {
     constructor( ){
@@ -9,9 +10,24 @@ export default class PortfolioManager extends Component {
 
         this.state = {
             portfolioItems: []
-        }
+        };
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         
-        }
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        //TODO
+        //update the portfolioItems state
+        //and add the portfolioItem to the lsit
+
+    }
+
+    handleFormSubmissionError(error) {
+        console.log('handleFormSubmissionError error', error);
+        
+    }
 
 
     getPortfolioItems() {
@@ -36,7 +52,10 @@ export default class PortfolioManager extends Component {
         return (
             <div className="portfolio-manager-wrapper">
                 <div className='left-column'>
-                    <h1>Portfolio form...</h1>
+                <PortfolioForm 
+                    handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+                    handleFormSubmissionError = {this.handleFormSubmissionError}
+                />
                 </div>
 
                 <div className='right-column'>
@@ -45,7 +64,7 @@ export default class PortfolioManager extends Component {
 
 
             </div>
-            );
-        }
+        );
+    }
     
 }
