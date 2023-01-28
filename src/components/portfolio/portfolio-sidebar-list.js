a494e820aed26ac2c0f612c083c2a125eab58061
DeviousWings <nksteele@byui.edu>
2023-01-28 14:17:32 -0500

    creating actual form and fixing key

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
+import React from "react";
 
-const PortfolioSidebarList = (props) => {
+const PortfolioSidebarList = props => {
+  const portfolioList = props.data.map(portfolioItem => {
+    return (
+      <div key={portfolioItem.id} className="portfolio-item-thumb">
+        <div className="portfolio-thumb-img">
+          <img src={portfolioItem.thumb_image_url} />
+        </div>
+        <h1 className="title">{portfolioItem.name}</h1>
+        <h2>{portfolioItem.id}</h2>
+      </div>
+    );
+  });
 
-    const portfolioList = props.data.map(portfolioItem => {
-        return (
-        <div className="portfolio-item-thumb">
-            <div className="portfolio-thumb-img">
-                <img src={portfolioItem.thumb_image_url}></img>
-            </div>
-            <h1 className='title'>{portfolioItem.name}</h1>
-            <h1>{portfolioItem.id}</h1>
-         </div>
-        )
-    })
-
-    return <div className='portfolio-sidebar-list-wrapper'>{portfolioList}</div>;
-}
+  return <div className="portfolio-sidebar-list-wrapper">{portfolioList}</div>;
+};
 
 export default PortfolioSidebarList;
