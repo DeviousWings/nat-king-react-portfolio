43a4651a9fc2492a54ad0335e3930300f5b882b3
DeviousWings <nksteele@byui.edu>
2023-02-13 16:22:19 -0500

    added blog-modal.js skelington

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
@@ -13,11 +14,19 @@ class Blog extends Component {
       totalCount: 0,
       currentPage: 0,
       isLoading: true,
+      blogModalIsOpen: false,
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
     this.onScroll = this.onScroll.bind(this);
     window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true,
+    });
   }
 
   onScroll() {
@@ -76,6 +85,11 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
+        <BlogModal modalIsOpen={this.state.blogModalIsOpen} />
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+        </div>
+
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
