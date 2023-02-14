6ba2c9194e66b6af53ab347c55b568771fe991b3
DeviousWings <nksteele@byui.edu>
2023-02-14 17:12:33 -0500

    authorized blog add

--- src/components/app.js
+++ src/components/app.js
@@ -121,11 +121,23 @@ export default class App extends Component {
 
               <Route path='/about-me' component={About} />
               <Route path='/contact' component={Contact} />
-              <Route path='/blog' component={Blog} />
+
+              <Route
+                path='/blog'
+                render={props => (
+                  <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+                )}
+              />
+
               <Route path='/b/:slug' component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN"
                 ? this.authorizedPages()
                 : null}
+              <Route
+                exact
+                path='/portfolio/:slug'
+                component={PortfolioDetail}
+              />
               <Route
                 exact2
                 path='/portfolio/:slug'
