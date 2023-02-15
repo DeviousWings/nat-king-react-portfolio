392e51ed8fb24c5b203aaa76d267e30be364cd78
DeviousWings <nksteele@byui.edu>
2023-02-14 21:43:49 -0500

    finish form

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -10,10 +10,17 @@ export default class BlogForm extends Component {
     this.state = {
       title: "",
       blog_status: "",
+      content: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleRichTextEditorChange =
+      this.handleRichTextEditorChange.bind(this);
+  }
+
+  handleRichTextEditorChange(content) {
+    this.setState({ content });
   }
 
   buildForm() {
@@ -21,6 +28,7 @@ export default class BlogForm extends Component {
 
     formData.append("portfolio_blog[title]", this.state.title);
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+    formData.append("portfolio_blog[content]", this.state.content);
 
     return formData;
   }
@@ -33,12 +41,13 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        this.props.handleSucFormSub(response.data.portfolio_blog);
-
         this.setState({
           title: "",
           blog_status: "",
+          content: "",
         });
+
+        this.props.handleSucFormSub(response.data.portfolio_blog);
       })
       .catch(error => {
         console.log("handleSubmit for blog error", error);
@@ -75,7 +84,9 @@ export default class BlogForm extends Component {
         </div>
 
         <div className='one-column'>
-          <RichTextEditor />
+          <RichTextEditor
+            handleRichTextEditorChange={this.handleRichTextEditorChange}
+          />
         </div>
         <button className='btn'>Save</button>
       </form>
