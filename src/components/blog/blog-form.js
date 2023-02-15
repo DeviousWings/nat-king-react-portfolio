47ed13a35113a0515e808a6cd1d036cbeff224c1
DeviousWings <nksteele@byui.edu>
2023-02-15 15:37:59 -0500

    able to put featured image up

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component";
 
 import RichTextEditor from "../forms/rich-text-editor";
 
@@ -11,12 +12,38 @@ export default class BlogForm extends Component {
       title: "",
       blog_status: "",
       content: "",
+      featured_image: "",
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleRichTextEditorChange =
       this.handleRichTextEditorChange.bind(this);
+
+    this.conponentConfig = this.componentConfig.bind(this);
+    this.djsConfig = this.djsConfig.bind(this);
+    this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+  }
+
+  componentConfig() {
+    return {
+      iconFiletypes: [".jpg", ".png"],
+      showFiletypeIcon: true,
+      postUrl: "https://httpbin.org/post",
+    };
+  }
+
+  djsConfig() {
+    return {
+      addRemoveLinks: true,
+      maxFiles: 1,
+    };
+  }
+
+  handleFeaturedImageDrop() {
+    return {
+      addedfile: file => this.setState({ featured_image: file }),
+    };
   }
 
   handleRichTextEditorChange(content) {
@@ -30,6 +57,13 @@ export default class BlogForm extends Component {
     formData.append("portfolio_blog[blog_status]", this.state.blog_status);
     formData.append("portfolio_blog[content]", this.state.content);
 
+    if (this.state.featured_image) {
+      formData.append(
+        "portfolio_blog[featured_image]",
+        this.state.featured_image
+      );
+    }
+
     return formData;
   }
 
@@ -41,13 +75,18 @@ export default class BlogForm extends Component {
         { withCredentials: true }
       )
       .then(response => {
+        if (this.state.featured_image) {
+          this.featuredImageRef.current.dropzone.removeAllFiles();
+        }
+
         this.setState({
           title: "",
           blog_status: "",
           content: "",
+          featured_image: "",
         });
 
-        this.props.handleSucFormSub(response.data.portfolio_blog);
+        this.propshandleSucFormSub(response.data.portfolio_blog);
       })
       .catch(error => {
         console.log("handleSubmit for blog error", error);
@@ -88,6 +127,16 @@ export default class BlogForm extends Component {
             handleRichTextEditorChange={this.handleRichTextEditorChange}
           />
         </div>
+
+        <div className='image-uploaders'>
+          <DropzoneComponent
+            config={this.componentConfig()}
+            djsConfig={this.djsConfig()}
+            eventHandlers={this.handleFeaturedImageDrop()}>
+            <div className='dz-message'>Featured Image</div>
+          </DropzoneComponent>
+        </div>
+
         <button className='btn'>Save</button>
       </form>
     );
