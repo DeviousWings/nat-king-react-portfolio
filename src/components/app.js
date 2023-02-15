5c3c6b9ee6113965edf5292bda85fa7a411a45d1
DeviousWings <nksteele@byui.edu>
2023-02-14 20:27:14 -0500

    added rxt

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from "axios";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faTrash,
-  faSignOutAlt,
-  faEdit,
-  faSpinner,
-  faPlusCircle,
-} from "@fortawesome/free-solid-svg-icons";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
@@ -21,12 +13,12 @@ import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
-
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
-
+import Icons from "../helpers/icons";
 export default class App extends Component {
   constructor(props) {
     super(props);
+
+    Icons();
 
     this.state = {
       loggedInStatus: "NOT_LOGGED_IN",
