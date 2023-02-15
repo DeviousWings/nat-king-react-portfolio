5641c5e6192e60bcdf5364d265102ff43e476b98
DeviousWings <nksteele@byui.edu>
2023-02-15 17:00:10 -0500

    fixed image feture issue/toolbar in add blog

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
+
 import BlogFeaturedImage from "../blog/blog-featured-image";
 export default class BlogDetail extends Component {
   constructor(props) {
@@ -35,16 +36,15 @@ export default class BlogDetail extends Component {
     const { title, content, featured_image_url, blog_status } =
       this.state.blogItem;
 
-    console.log("currentId", this.state.currentId);
     return (
       <div className='blog-container'>
         <div className='content-container'>
           <h1>{title}</h1>
+
+          <BlogFeaturedImage img={featured_image_url} />
+
+          <div className='content'>{ReactHtmlParser(content)}</div>
         </div>
-
-        <BlogFeaturedImage img={featured_image_url} />
-
-        <div className='content'>{ReactHtmlParser(content)}</div>
       </div>
     );
   }
