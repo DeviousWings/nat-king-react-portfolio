3c90b60d7b535357a9318b253f85b21fe9681ae2
DeviousWings <nksteele@byui.edu>
2023-02-15 21:02:28 -0500

    updated about/contact

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -4,11 +4,23 @@ import {
   faEdit,
   faSpinner,
   faPlusCircle,
+  faPhone,
+  faEnvelope,
+  faMapMarkedAlt,
 } from "@fortawesome/free-solid-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
-  return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
+  return library.add(
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+  );
 };
 
 export default Icons;
