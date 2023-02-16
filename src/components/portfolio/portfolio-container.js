3915336c2b843bcb8b4f0943570b6ecee91e55c4
DeviousWings <nksteele@byui.edu>
2023-02-16 18:34:32 -0500

    fixed filter for home

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,27 +10,35 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: []
+      data: [],
     };
 
     this.handleFilter = this.handleFilter.bind(this);
   }
 
   handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.category === filter;
-      })
-    });
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
+    }
   }
 
-  getPortfolioItems() {
+  getPortfolioItems(filter = null) {
     axios
       .get("https://natking.devcamp.space/portfolio/portfolio_items")
       .then(response => {
-        this.setState({
-          data: response.data.portfolio_items
-        });
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter(item => {
+              return item.category === filter;
+            }),
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items,
+          });
+        }
       })
       .catch(error => {
         console.log(error);
@@ -53,18 +61,28 @@ export default class PortfolioContainer extends Component {
     }
 
     return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
-          Contract
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
-          Private Sector
-        </button>
-        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
-          Goverment
-        </button>
-
-        {this.portfolioItems()}
+      <div className='homepage-wrapper'>
+        <div className='filter-links'>
+          <button
+            className='btn'
+            onClick={() => this.handleFilter("Goverment")}>
+            Goverment
+          </button>
+          <button
+            className='btn'
+            onClick={() => this.handleFilter("Private Sector")}>
+            Private Sector
+          </button>
+          <button className='btn' onClick={() => this.handleFilter("Contract")}>
+            Contract
+          </button>
+          <button
+            className='btn'
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}>
+            All
+          </button>
+        </div>
+        <div className='portfolio-items-wrapper'>{this.portfolioItems()}</div>
       </div>
     );
   }
